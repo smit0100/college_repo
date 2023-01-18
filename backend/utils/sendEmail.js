aa39885626a7cb26a4fb1824a415a61da828feae
smit0100 <smitdankhra86@gmail.com>
2023-01-18 18:50:36 +0530

    email veriable change

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,23 +1,23 @@
 const nodemailer = require('nodemailer');
 
-
+require('dotenv').config()
   
 
 module.exports = async (email, subject, text) => {
     
 
 let transporter = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
+    host: process.env.HOST,
+    port: process.env.EMAIL_PORT,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'smitdankhra01@gmail.com', // generated ethereal user
-      pass: 'rahSqIfVBNtmdMOP', // generated ethereal password
+      user: process.env.USER, // generated ethereal user
+      pass: process.env.PASSPORT, // generated ethereal password
     },
 });
     
     let info = await transporter.sendMail({
-        from: '"Fred Foo 👻" <smitdankhra01@gmail.com>', // sender address
+        from: process.env.USER, // sender address
         to: email, // list of receivers
         subject: subject, // Subject line
         text: text, // plain text body
