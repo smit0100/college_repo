97979641ecdfefcb51f53f426fa0cd653705f980
smit0100 <smitdankhra86@gmail.com>
2023-01-21 16:35:09 +0530

    error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../controller/productController');
 
 router.post('/add', productController.createProduct);
+router.get('/fetchAll', productController.fetchAllProduct);
 
 module.exports = router
